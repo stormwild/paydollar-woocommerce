c99c79a3afd51dddd97d02a1a830a5db9c64bbe0
Alexander R. Torrijos <artorrijos@gmail.com>
2016-04-11 14:47:46 +0800

    Refactored payment form script

---
+++ js/paydollar.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+    $(function(){
+        setTimeout(paydollar_payment_form, 5000);
+    });
+    
+    function paydollar_payment_form(){
+        $('#paydollar_payment_form').submit();
+    }
+
+})(jQuery);
